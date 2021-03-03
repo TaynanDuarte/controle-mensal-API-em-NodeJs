5f5f580fed10be893e3b99aaa02efd5d21616657
TaynanDuarte <taynan_duarte@outlook.com>
2021-03-03 04:34:10 -0300

    time_tools minutsToSeconds tested

--- src/utils/time_tools.js
+++ src/utils/time_tools.js
@@ -1,13 +1,17 @@
 
 
 function minutsToSeconds(minuts) {
-
     if (!Number.isInteger(minuts)) throw new Error('minuts parameter should be a integer');
-
     return minuts * 60;
+}
+
+function secondsToMinuts(seconds) {
+    if (!Number.isInteger(seconds)) throw new Error('seconds parameter should be a integer');
+    return seconds / 60;
 }
 
 
 module.exports = {
-    minutsToSeconds
+    minutsToSeconds,
+    secondsToMinuts
 }
--- test/unit/utils.test/time_tools.test.js
+++ test/unit/utils.test/time_tools.test.js
@@ -2,13 +2,30 @@ const timeTools = require('../../../src/utils/time_tools');
 
 describe('time_tools test', () => {
 
+    describe('secondsToMinuts', () => {
+
+        it('should recive seconds and retun minuts', () => {
+
+            const seconds = 300;
+            const minuts = timeTools.secondsToMinuts(seconds);
+            expect(minuts).toBe(5);
+
+        });
+
+        it('should throw a error when parameter is not a integer', () => {
+            const seconds = "5";
+            expect(() => timeTools.secondsToMinuts(seconds)).toThrow('seconds parameter should be a integer');
+        });
+
+    });
+
     describe('minutsToSeconds', () => {
 
         it('should recive minuts and retun seconds', () => {
 
-            const minuts = 5;
-            const seconds = timeTools.minutsToSeconds(minuts);
-            expect(seconds).toBe(300);
+            const minuts = 300;
+            const seconds = timeTools.secondsToMinuts(minuts);
+            expect(seconds).toBe(5);
 
         });
 
