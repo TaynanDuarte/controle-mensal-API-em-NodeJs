9a6e4e780b3bfc627dfb9d45c3d2e54a928b93be
TaynanDuarte <taynan_duarte@outlook.com>
2021-03-05 02:22:20 -0300

    userController createUser

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,15 +4,20 @@ const userFactory = require('../factories/user.factory')();
 
 function userController() {
 
-    const userService = userFactory.userServiceFactory()
+    const userService = userFactory.userServiceFactory();
 
     async function getUser(email, password) {
         return await userService.getUser(email, password);
     }
 
+    async function createUser(user) {
+        return await userService.createUser(user);
+    }
+
 
     return {
-        getUser
+        getUser,
+        createUser
     }
 
 }
