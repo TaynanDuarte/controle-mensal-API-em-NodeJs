cdb6f50ee9f52529a812afd1dbc8fe727ec19862
TaynanDuarte <taynan_duarte@outlook.com>
2021-03-05 00:11:23 -0300

    user.schema - updateAt added

--- src/database/schemas/user.schema.js
+++ src/database/schemas/user.schema.js
@@ -5,7 +5,8 @@ const UserSchema = mongoose.Schema({
     name: { type: String, require: true },
     email: { type: String, require: true, unique: true },
     password_hash: { type: String, require: true, unique: true },
-    role: { type: String, require: true }
+    role: { type: String, require: true },
+    updatedAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('users', UserSchema);
