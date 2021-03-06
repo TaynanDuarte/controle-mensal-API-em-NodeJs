65d99ec3fc8239201cd2587f46efddd8ddbfa4c8
TaynanDuarte <taynan_duarte@outlook.com>
2021-03-06 01:52:44 -0300

    src/server.js dotenv configured

--- src/server.js
+++ src/server.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
+
 const app = express();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended:false }));
+
+app.disable('x-powered-by');
 
 
 const userController = require('./controller/user.controller')();
 
 app.get('/', (req, res) => {
     return res.send('*')
-})
+});
 
 app.post('/login', (req, res) => {
 
     return res.send('aaaa');
-})
+});
+
+app.post('/register', (req, res) => {
+
+});
 
 
 app.listen(3000);
 
+console.log('>>>>>>>>>> ', process.env.NODE_ENV);
 console.log('Server running on port 3000');
