cdae57b81922355fc7caa1b3b65f1f8de01a81f0
TaynanDuarte <taynan_duarte@outlook.com>
2021-03-07 06:44:47 -0300

    cors configured

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-
 require('dotenv').config({
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 });
 
+const express = require('express');
+const bodyparser = require('body-parser');
+const corsConfig = require('./cors.config');
+
 const app = express();
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended:false }));
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(corsConfig);
 
 app.disable('x-powered-by');
 
@@ -24,11 +26,11 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-
+    console.log('>>>>>>>>>> ', req.body);
 });
 
 
-app.listen(3000);
+app.listen(3001);
 
 console.log('>>>>>>>>>> ', process.env.NODE_ENV);
-console.log('Server running on port 3000');
+console.log('Server running on port 3001');
